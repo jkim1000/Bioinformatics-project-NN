f3a93ecc081a7231098ab51bdbd1d89b00d999bb
jkim1000 <67875208+jkim1000@users.noreply.github.com>
2021-06-09 11:24:47 -0400

    Update app.py

--- bioactivity-prediction-app/app.py
+++ bioactivity-prediction-app/app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-# from rdkit import Chem
+from rdkit import Chem
 # from rdkit.Chem.Draw import IpythonConsole
-# from rdkit.Chem import Draw
+from rdkit.Chem import Draw
 import numpy as np
-# from rdkit.Chem import MACCSkeys, Draw
+from rdkit.Chem import MACCSkeys, Draw
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.callbacks import ModelCheckpoint
